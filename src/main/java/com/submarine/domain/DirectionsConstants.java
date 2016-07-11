4e207a52fbf26a5abbdd4df0a9a49d38e7829444
Fabio Cunha <fabio@Fabios-MacBook-Pro.local>
2016-07-10 22:57:55 -0300

    refactor das constantes

--- src/main/java/com/submarine/domain/DirectionsConstants.java
+++ src/main/java/com/submarine/domain/DirectionsConstants.java
@@ -2,8 +2,8 @@ package com.submarine.domain;
 
 public class DirectionsConstants {
 
-    public static String NORTH = "NORTH";
-    public static String SOUTH = "SOUTH";
-    public static String EAST = "EAST";
-    public static String WEST = "WEST";
+    public static final String NORTH = "NORTH";
+    public static final String SOUTH = "SOUTH";
+    public static final String EAST = "EAST";
+    public static final String WEST = "WEST";
 }
